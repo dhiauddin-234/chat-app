9cdfaf1416ded6d25102339160964d2285c77a49
dhiauddin2010 <dhiauddin2010@gmail.com>
2025-11-15 10:20:26 +0000

    Remove login and registration pages

--- chat-app/client/src/App.js
+++ chat-app/client/src/App.js
@@ -1,35 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import Landing from './components/Landing';
-import Register from './components/Register';
 import Chat from './components/Chat';
 import RoomList from './components/RoomList';
-import ProtectedRoute from './components/ProtectedRoute';
-import authService from './services/authService';
-import './App.css';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/register" element={<Register />} />
-        <Route 
-          path="/rooms" 
-          element={
-            <ProtectedRoute isAllowed={authService.isAuthenticated()}>
-              <RoomList />
-            </ProtectedRoute>
-          }
-        />
-        <Route 
-          path="/chat/:roomId" 
-          element={
-            <ProtectedRoute isAllowed={authService.isAuthenticated()}>
-              <Chat />
-            </ProtectedRoute>
-          }
-        />
+        <Route path="/" element={<Navigate to="/rooms" />} />
+        <Route path="/rooms" element={<RoomList />} />
+        <Route path="/chat/:roomId" element={<Chat />} />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Router>
